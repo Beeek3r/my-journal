4798adbe92ba2de903593b771f1e2dbd1d12d503
beeek3r <baker.xu39@gmail.com>
2019-10-21 20:16:54 +1000

    sass

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import React from 'react';
+import './app.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import Login from './components/auth/Login'
-import Register from './components/auth/Register'
-import PrivateRoute from './components/routing/PrivateRoute'
-import AuthState from './context/auth/AuthState'
-import JournalState from './context/journal/JournalState'
-import NotFound from './components/views/notFound/NotFound'
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import PrivateRoute from './components/routing/PrivateRoute';
+import AuthState from './context/auth/AuthState';
+import JournalState from './context/journal/JournalState';
+import NotFound from './components/views/notFound/NotFound';
 
-import Home from './components/views/home/Home'
-import Journal from './components/views/journal/Journal'
-import JournalNew from './components/views/journal-new/JournalNew'
-import JournalEdit from './components/views/journal-edit/JournalEdit'
+import Home from './components/views/home/Home';
+import Journal from './components/views/journal/Journal';
+import JournalNew from './components/views/journal-new/JournalNew';
+import JournalEdit from './components/views/journal-edit/JournalEdit';
 
 function App() {
   return (
@@ -33,9 +33,9 @@ function App() {
         </JournalState>
       </AuthState>
     </Router>
-  )
+  );
 }
 
 // Remember to delete to test css file sass etc.
 
-export default App
+export default App;
